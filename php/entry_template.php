d5d81cd9a6d435baf2d25f2c265edcd271d8f761
nathanielwinckler <nathanielwinckler@mac-pro.local>
2010-02-12 19:53:02 -0500

    Function changes: NW.io.getFilesArray() NW.io.save() NW.io.publishPage() NEW: NW.editor.functions.getFieldsDataArray()
    Updated entry_template.php

--- php/entry_template.php
+++ php/entry_template.php
@@ -3,8 +3,8 @@
         <title><?php echo $data['title']; ?></title>
     </head>
     <body>
-        <div class="NWEditable" id="title"><?php echo $data['title']; ?></div>
-        <div class="NWEditable" id="body"><?php echo $data['body']; ?></div>
-        <div class="NWEditable" id="author"><?php echo $data['author']; ?></div>
+        <div class="NWEditable NWFieldtitleNWZ" id="title"><?php echo $data['title']; ?></div>
+        <div class="NWEditable NWFieldbodyNWZ" id="body"><?php echo $data['body']; ?></div>
+        <div class="NWEditable NWFieldauthorNWZ" id="author"><?php echo $data['author']; ?></div>
     </body>
 </html>
