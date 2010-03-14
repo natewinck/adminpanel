d62970f78b7e5c87b07dd829f2de1320b0bf3353
J. D. Smith <emallson@archlinux.us>
2010-03-14 18:06:34 -0400

    New Database Layout w/ associated PHP and JS mods. Saver has been disabled to prevent corruption. It will be rewritten and reenabled.

--- php/loader.php
+++ php/loader.php
@@ -2,13 +2,23 @@
     require "mysql_backend.php";
     
     //print_r($_GET);
-    if(isset($_GET['id']) && is_numeric($_GET['id']) && isset($_GET['cat']))
+    if(isset($_GET['id']) && is_numeric($_GET['id']) && isset($_GET['cat'])) //Get a single Entry
     {
-        $con = get_connection();
+        $con = get_connection(); 
         $data = get_entry($con, $_GET['cat'], 'id', $_GET['id']);
+		if($data['data'] != NULL)
+		{
+			$data_col = unserialize(base64_decode($data['data'])); //Data array in the DB is serialized and B64d, so we need to undo it.
+			unset($data['data']); //Remove the B64 version
+			$data = array_merge($data, $data_col); //Merge the new arrays
+		}
+		else
+		{
+			unset($data['data']);
+		}
         include("entry_template.php");
     }
-    else if(isset($_GET['cat']) && isset($_GET['xml']))
+    else if(isset($_GET['cat']) && isset($_GET['xml'])) //Get list of entries in XML form
     {
         $con = get_connection();
         if(isset($_GET['start']) && isset($_GET['max']))
@@ -23,9 +33,22 @@
         {
             $data = get_entries($con, $_GET['cat']);
         }
+        foreach($data as $row)
+        {
+			if($row['data'] != NULL)
+			{
+				$data_col = unserialize(base64_decode($row['data'])); //Data array in the DB is serialized and B64d, so we need to undo it.
+				unset($row['data']); //Remove the B64 version
+				$data = array_merge($row, $data_col); //Merge the new arrays
+			}
+			else
+			{
+				unset($row['data']);
+			}
+        }
         include("entries_data_template.php");
     }
-    else if(isset($_GET['cat']))
+    else if(isset($_GET['cat'])) //Get a list of entries
     {
         $con = get_connection();
         if(isset($_GET['start']) && isset($_GET['max']))
@@ -42,16 +65,16 @@
         }
         //include("entries_template.php");
     }
-    else if(isset($_GET['xml']))
+    else if(isset($_GET['xml'])) //Get a list of pages in XML form
     {
         $con = get_connection();
-        $data = get_categories($con);
-        include("categories_data_template.php");
+        $data = get_pages($con);
+        include("pages_data_template.php");
     }
-    else
+    else //Get a list of pages
     {
         $con = get_connection();
-        $data = get_categories($con);
-        include("categories_template.php");
+        $data = get_pages($con);
+        include("pages_template.php");
     }
 ?>
