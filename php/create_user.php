8fbb896b918ba253dcd80a16fb0929c023346dd3
nathanielwinckler <nathanielwinckler@mac-pro.local>
2010-06-09 00:03:05 -0400

    Made the login system work with the new encryption. Made editing the password work. Added a utils.js file.

--- php/create_user.php
+++ php/create_user.php
@@ -8,7 +8,7 @@
 	$email = mysql_real_escape_string($_GET['email']);
 	$rank = mysql_real_escape_string($_GET['rank']);
 	
-	$password = hash('sha256', hash('sha512', hash('sha256', $password) . "boom-deyada boom-deyada"));
+	$password = raw_hash($password);
 	
 	echo insert_user_data($con, $username, $password, $email, $rank);
 ?>
